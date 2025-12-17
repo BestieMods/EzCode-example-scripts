ca8e591f52cdd5f3c3eada74a4ea23f9ed18587f
ItsBestie <Whittdesk@outlook.com>
2025-12-17 11:05:42 -0500

    Made a variables and math folder with a script

---
+++ code-folder/python/Simple/variables-and-math/Simple-var-script-1.py
@@ -0,0 +1,15 @@
+# variables are used to store one piece of info. Like an integer, float, or string
+# if you want to add a variable, just do like x = # or x = "string here"
+variable_1 = "string"
+variable_2 = "BestieMods"
+
+print(variable_1)
+print(variable_2)
+
+# The code should print as in the comment below
+"""
+string
+Bestiemods
+"""
+
+# Change up some stuff here to let you say anything :P
