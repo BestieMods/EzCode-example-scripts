5aba67519856fc9520af1eec236c860c9304c6de
ItsBestie <Whittdesk@outlook.com>
2025-12-17 11:22:35 -0500

    Implement simple math operations and output
    Added basic arithmetic operations and print statements.

---
+++ code-folder/python/Simple/variables-and-math/Simple-Math-example.py
@@ -0,0 +1,16 @@
+# you can see the results in the terminal!
+adding = 3 + 2
+subtracting = 5 - 2
+multiplication = 3*2
+division = 8/2
+
+print(adding)
+print(subtracting)
+print(multiplication)
+print(division)
+
+# If works correctly, it should print out like below
+# 5
+# 3
+# 6
+# 4
