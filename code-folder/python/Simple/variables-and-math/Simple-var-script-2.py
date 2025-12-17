9070d8760bc6a72e83451f7770894ef83235a766
ItsBestie <Whittdesk@outlook.com>
2025-12-17 11:12:21 -0500

    created script

---
+++ code-folder/python/Simple/variables-and-math/Simple-var-script-2.py
@@ -0,0 +1,6 @@
+# In this example we can use it to make a story by changing variables
+place = "Beach"
+person = "BestieMods"
+thing = "Burger"
+print("Today, " + person + " was at the " + place + " eating a " + thing + ".")
+# You can change up the variables or add some to make a story :P
